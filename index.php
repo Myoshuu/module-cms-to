9fe70092e06756a553bfc862ad876375633a5ce5
Joevano <myoshu.me@gmail.com>
2022-06-25 21:12:51 +0700

    show post type news

--- index.php
+++ index.php
@@ -3,4 +3,26 @@
   <div class="image"></div>
   <div class="overlay">Ayo lawan Corona!</div>
 </div>
+
+<div class="container">
+  <div class="news">
+    <h3>News</h3>
+    <div>
+      <?php $query = new WP_Query(['post_type' => 'news']) ?>
+      <?php if ($query->have_posts()) : ?>
+        <?php while ($query->have_posts()) : $query->the_post() ?>
+          <div>
+            <div class="left">
+              <img src="<?= get_the_post_thumbnail_url() ?>" alt="<?= the_title() ?>">
+            </div>
+            <div class="right">
+              <h3 class="mb-1"><a href="<?= the_permalink() ?>"><?= the_title() ?></a></h3>
+              <p><?= the_excerpt() ?></p>
+            </div>
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
 <?= get_footer() ?>
