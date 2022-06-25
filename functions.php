cbd805485d5fbf0ce27c0ef35b5c170d72dfed81
Joevano <myoshu.me@gmail.com>
2022-06-25 20:01:15 +0700

    new functions (register post type) copy media files

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+define('@', get_stylesheet_directory_uri());
+
+add_action('init', function () {
+  register_post_type('news', [
+    'label' => 'News',
+    'public' => true,
+    'supports' => ['title', 'editor', 'thumbnail'],
+    'taxonomies' => ['category']
+  ]);
+
+  register_post_type('events', [
+    'label' => 'Events',
+    'public' => true,
+    'supports' => ['title', 'editor', 'thumbnail'],
+    'taxonomies' => ['category']
+  ]);
+});
